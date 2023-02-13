e2217b6b922d1412d4c443cf5b8cfa7c4c6d58c9
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2023-02-13 16:30:14 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <p className="hello">Hello,</p>
-          <p className="hello">world!</p>
+          <div className="center">
+            <p className="hello">Hello,</p>
+            <p className="hello">world!</p>
+          </div>
         </div>
       </header>
     </div>
